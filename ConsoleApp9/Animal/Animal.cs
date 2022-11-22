d22efa507ebbdcf15c9c857a1fd14184d56bdd1e
tworal02 <tworal02@gmail.com>
2022-11-22 00:46:26 -0800

    Add project files.

---
+++ ConsoleApp9/Animal/Animal.cs
@@ -0,0 +1,47 @@
+﻿using ConsoleApp9.option;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp9
+{
+    public abstract class AbstractAnimal
+    {
+        public string Species;
+        public string Biome;
+        public int Area;
+        public string Meal;
+        public string Sort;
+        public string Sound;
+        public string Name;
+        public double FoodAmount;
+        public int age;
+        public BiomType biom;
+        public double area;
+        public AbstractAnimal(string name, int Age)
+        {
+            Name = name;
+            age = Age;
+        }
+
+        public void Eat()
+        {
+            Console.WriteLine("nyam nyam");
+        }
+
+        public void MakeSound()
+        {
+            Console.WriteLine("auuu!");
+        }
+
+        public void Play()
+        {
+            Console.WriteLine("I'm running");
+        }
+        
+    }
+    
+}
---
+++ ConsoleApp9/Zebra.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp9
+{
+    public class Zebra : AbstractAnimal
+
+    {
+        public Zebra(string name, int age): base(name, age)
+        {
+            Name ="Martin";
+            age = 10;
+      
+        }
+        
+    }
+}
